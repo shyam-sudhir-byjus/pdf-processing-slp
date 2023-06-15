e6e116720a40b9c36b983364113c9d60def971e0
shyam-sudhir-byjus <shyam.sudhir@byjus.com>
2023-06-15 12:36:26 +0530

    fix

--- find_maths_science_in_folders.py
+++ find_maths_science_in_folders.py
@@ -1,24 +1,22 @@
 import os
 import re
+import shutil
 
-def find_files_with_keywords(directory, keywords):
+def create_directory_with_matching_files(source_directory, target_directory, keywords):
     pattern = re.compile(r"|".join(keywords), re.IGNORECASE)
-    matching_files = []
 
-    for root, dirs, files in os.walk(directory):
-        for f in files:
-            if pattern.search(f):
-                matching_files.append(os.path.join(root, f))
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
 
-    return matching_files
+    for root, dirs, files in os.walk(source_directory):
+        for file in files:
+            if pattern.search(file):
+                source_path = os.path.join(root, file)
+                target_path = os.path.join(target_directory, file)
+                shutil.copy2(source_path, target_path)
 
+source_directory_path = "pdf-processing-slp/data_respaper_text_links"
+target_directory_path = "pdf-processing-slp/data_respaper_text_links_maths_science"
+keywords = ["Science", "Maths","Biology", "Chemistry", "Physics", "cbse"]
 
-if __name__ == "__main__":
-    directory_path = "pdf-processing-slp/data_respaper_text_links"
-
-    keywords = ["Science","Maths","Chemistry","Physics","Biology","cbse"]
-    matching_files = find_files_with_keywords(directory_path, keywords)
-
-    print("Matching files:")
-    for f in matching_files:
-        print(f)
+create_directory_with_matching_files(source_directory_path, target_directory_path, keywords)
